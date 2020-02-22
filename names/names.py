bbb3eb532f527d1cf57a741debf3e22ae3bd96ca
Tim Dill <brodt7258@gmail.com>
2020-02-21 23:55:02 -0500

    finding dupes in 0.25 sec

--- names/names.py
+++ names/names.py
@@ -26,10 +26,16 @@ print('tree_2 assembled')
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+def append_dupes(item):
+    if names_tree_2.contains(item):
+        duplicates.append(item)
+
+names_tree_1.for_each(append_dupes)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
