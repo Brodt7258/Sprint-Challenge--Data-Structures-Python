0057bbc621573540307ed87b050c1e36154c0fd3
Tim Dill <brodt7258@gmail.com>
2020-02-22 01:16:11 -0500

    ArrayRingBuffer implementation

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -14,13 +14,17 @@ class RingBuffer:
             yield item.value
 
     def append(self, item):
+        # not yet at capacity? append to tail
         if len(self.storage) < self.capacity:
             self.storage.add_to_tail(item)
             
+            # initialize self.current when adding the first item to the list
             if len(self.storage) == 1:
                 self.current = self.storage.tail
         else:
+            # list is full, start overwritting the oldest element
             self.current.value = item
+            # move current to the next oldest. loop back to the head upon reaching the tail
             self.current = self.current.next if self.current.next else self.storage.head
 
     def get(self):
@@ -33,13 +37,17 @@ class RingBuffer:
 
 # ----------------Stretch Goal-------------------
 
+# this just seems like a superior way to implement this structure?
+# doing it with a linked list kinda sucked. A lot of overhead for seemingly no payoff
 
 class ArrayRingBuffer:
     def __init__(self, capacity):
-        pass
+        self.current = 0
+        self.storage = [None] * capacity
 
     def append(self, item):
-        pass
+        self.storage[self.current] = item
+        self.current = self.current + 1 if self.current < len(self.storage) - 1 else 0
 
     def get(self):
-        pass
+        return [item for item in self.storage if item is not None]
